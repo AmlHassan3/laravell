7ea652506987fc061b2f5c61c8d24c31bd848c6f
Your Name <you@example.com>
2023-05-06 16:05:34 +0300

    added departments

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Department;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +15,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Department::create([
+            'name' => 'Information System',
+            'code' => 'IS'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        Department::create([
+            'name' => 'Information Technology',
+            'code' => 'IT'
+        ]);
+
+
+        Department::create([
+            'name' => 'Computr Science',
+            'code' => 'CS'
+        ]);
     }
 }
